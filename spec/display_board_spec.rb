fb34bf547fd5b0037fe74a5854d8034a99afd1a4
bold-firmware-1755 <leonora.squires@gmail.com>
2020-02-11 22:14:20 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -178,7 +178,7 @@ describe "#display_board in 'lib/display_board.rb" do
     it 'prints an entire board full of Os' do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
-      output = capture_puts{ display_board(board) }
+       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
       expect(rows[0]).to eq(" O | O | O ")
